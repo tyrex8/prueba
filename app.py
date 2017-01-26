855a8049f8e1a45ba56bdbf69b28015b55a77031
tyrex8 <sgalicia88@gmail.com>
2017-01-26 15:36:39 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -33,10 +33,9 @@ def webhook():
 
 def processRequest(req):
     if req.get("result").get("action") == "Posicion":
-		res = makeWebhookResult()
-		return res
-	else:
-		return {}
+			res = makeWebhookResult()
+			return res
+	else: return {}
 
 
 
